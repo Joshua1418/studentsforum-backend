0ae21e4b3537047a26e9f48d520c3d0bccf467ae
Joshua1418 <joshuanzanza@gmail.com>
2024-07-25 03:03:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require("express")
+var cors = require("cors")
+const mysql = require("mysql")
+
+var app = express()
+app.use(cors())
+app.use(express.json())
+
+let db=mysql.createConnection({
+  host:'localhost',
+  user:"root",
+  database:"forum_db",
+  password:""
+})
+
+app.post("/add",(req,res)=>{ 
+  db.query(`INSERT INTO forum_info VALUES(0, '${req.body.nom}','${req.body.message}')`, (erreur,result) => {
+    return res.json();
+  })  
+})
+
+
+app.get("/messages",(req,res)=>{ 
+  db.query(`SELECT * FROM forum_info ORDER BY id ASC`, (erreur,result) => {
+  return res.json(result);
+  })  
+})
+
+app.post("/delete",(req,res)=>{ 
+  db.query(`DELETE FROM forum_info WHERE id = '${req.body.id}'`, (erreur,result) => {
+  return res.json(result);
+  })  
+})
+ 
+ 
+
+app.listen(4000, ()=>{
+  console.log("le serveur a démarré au port 3000");
+})
